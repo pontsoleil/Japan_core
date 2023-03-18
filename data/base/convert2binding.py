9f44b59bd594780845def9888798c6160a6703ae
Nobuyuki SAMBUICHI <nobuyuki@sambuichi.jp>
2023-03-19 07:44:55 +0900

    Update convert2binding.py

--- data/base/convert2binding.py
+++ data/base/convert2binding.py
@@ -27,8 +27,6 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
-import xml.etree.ElementTree as ET
-from collections import defaultdict
 import csv
 import os
 
