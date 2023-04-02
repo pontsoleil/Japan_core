ceeb003ed409f166e76e68d2db7e36256d491b0f
SAMBUICHI, Nobuyuki <nobuyuki@sambuichi.jp>
2023-04-02 15:59:02 +0900

    Create test.sh

---
+++ test.sh
@@ -0,0 +1,48 @@
+# 
+java -classpath core-japan-0.0.1.jar wuwei.japan_core.cius.Invoice2csv JP-PINT data/xml/JP-PINT/Example.xml data/csv/JP-PINT/Example.csv T
+java -classpath core-japan-0.0.1.jar wuwei.japan_core.cius.Invoice2csv JP-PINT data/xml/JP-PINT/Example1-minimum.xml data/csv/JP-PINT/Example1-minimum.csv T
+java -classpath core-japan-0.0.1.jar wuwei.japan_core.cius.Invoice2csv JP-PINT data/xml/JP-PINT/Example2-TaxAcctCur.xml data/csv/JP-PINT/Example2-TaxAcctCur.csv T
+java -classpath core-japan-0.0.1.jar wuwei.japan_core.cius.Invoice2csv JP-PINT data/xml/JP-PINT/Example3-SumInv1.xml data/csv/JP-PINT/Example3-SumInv1.csv T
+java -classpath core-japan-0.0.1.jar wuwei.japan_core.cius.Invoice2csv JP-PINT data/xml/JP-PINT/Example4-SumInv2.xml data/csv/JP-PINT/Example4-SumInv2.csv T
+java -classpath core-japan-0.0.1.jar wuwei.japan_core.cius.Invoice2csv JP-PINT data/xml/JP-PINT/Example5-AllowanceCharge.xml data/csv/JP-PINT/Example5-AllowanceCharge.csv T
+java -classpath core-japan-0.0.1.jar wuwei.japan_core.cius.Invoice2csv JP-PINT data/xml/JP-PINT/Example6-CorrInv.xml data/csv/JP-PINT/Example6-CorrInv.csv T
+java -classpath core-japan-0.0.1.jar wuwei.japan_core.cius.Invoice2csv JP-PINT data/xml/JP-PINT/Example7-Return.Quan.ItPr.xml data/csv/JP-PINT/Example7-Return.Quan.ItPr.csv T
+java -classpath core-japan-0.0.1.jar wuwei.japan_core.cius.Invoice2csv JP-PINT data/xml/JP-PINT/Example9-SumInv1andO.xml data/csv/JP-PINT/Example9-SumInv1andO.csv T
+
+java -classpath core-japan-0.0.1.jar wuwei.japan_core.cius.Csv2invoice JP-PINT data/csv/JP-PINT/Example.csv data/xml/JP-PINT2/Example.xml T
+java -classpath core-japan-0.0.1.jar wuwei.japan_core.cius.Csv2invoice JP-PINT data/csv/JP-PINT/Example1-minimum.csv data/xml/JP-PINT2/Example1-minimum.xml T
+java -classpath core-japan-0.0.1.jar wuwei.japan_core.cius.Csv2invoice JP-PINT data/csv/JP-PINT/Example2-TaxAcctCur.csv data/xml/JP-PINT2/Example2-TaxAcctCur.xml T
+java -classpath core-japan-0.0.1.jar wuwei.japan_core.cius.Csv2invoice JP-PINT data/csv/JP-PINT/Example3-SumInv1.csv data/xml/JP-PINT2/Example3-SumInv1.xml T
+java -classpath core-japan-0.0.1.jar wuwei.japan_core.cius.Csv2invoice JP-PINT data/csv/JP-PINT/Example4-SumInv2.csv data/xml/JP-PINT2/Example4-SumInv2.xml T
+java -classpath core-japan-0.0.1.jar wuwei.japan_core.cius.Csv2invoice JP-PINT data/csv/JP-PINT/Example5-AllowanceCharge.csv data/xml/JP-PINT2/Example5-AllowanceCharge.xml T
+java -classpath core-japan-0.0.1.jar wuwei.japan_core.cius.Csv2invoice JP-PINT data/csv/JP-PINT/Example6-CorrInv.csv data/xml/JP-PINT2/Example6-CorrInv.xml T
+java -classpath core-japan-0.0.1.jar wuwei.japan_core.cius.Csv2invoice JP-PINT data/csv/JP-PINT/Example7-Return.Quan.ItPr.csv data/xml/JP-PINT2/Example7-Return.Quan.ItPr.xml T
+java -classpath core-japan-0.0.1.jar wuwei.japan_core.cius.Csv2invoice JP-PINT data/csv/JP-PINT/Example9-SumInv1andO.csv data/xml/JP-PINT2/Example9-SumInv1andO.xml T
+
+java -classpath core-japan-0.0.1.jar wuwei.japan_core.cius.Invoice2csv SME-COMMON data/xml/JP-PINT/Example.xml data/csv/SME-COMMON/Example.csv T
+java -classpath core-japan-0.0.1.jar wuwei.japan_core.cius.Invoice2csv SME-COMMON data/xml/JP-PINT/Example1-minimum.xml data/csv/SME-COMMON/Example1-minimum.csv T
+java -classpath core-japan-0.0.1.jar wuwei.japan_core.cius.Invoice2csv SME-COMMON data/xml/JP-PINT/Example2-TaxAcctCur.xml data/csv/SME-COMMON/Example2-TaxAcctCur.csv T
+java -classpath core-japan-0.0.1.jar wuwei.japan_core.cius.Invoice2csv SME-COMMON data/xml/JP-PINT/Example3-SumInv1.xml data/csv/SME-COMMON/Example3-SumInv1.csv T
+java -classpath core-japan-0.0.1.jar wuwei.japan_core.cius.Invoice2csv SME-COMMON data/xml/JP-PINT/Example4-SumInv2.xml data/csv/SME-COMMON/Example4-SumInv2.csv T
+java -classpath core-japan-0.0.1.jar wuwei.japan_core.cius.Invoice2csv SME-COMMON data/xml/JP-PINT/Example5-AllowanceCharge.xml data/csv/SME-COMMON/Example5-AllowanceCharge.csv T
+java -classpath core-japan-0.0.1.jar wuwei.japan_core.cius.Invoice2csv SME-COMMON data/xml/JP-PINT/Example6-CorrInv.xml data/csv/SME-COMMON/Example6-CorrInv.csv T
+java -classpath core-japan-0.0.1.jar wuwei.japan_core.cius.Invoice2csv SME-COMMON data/xml/JP-PINT/Example7-Return.Quan.ItPr.xml data/csv/SME-COMMON/Example7-Return.Quan.ItPr.csv T
+java -classpath core-japan-0.0.1.jar wuwei.japan_core.cius.Invoice2csv SME-COMMON data/xml/JP-PINT/Example9-SumInv1andO.xml data/csv/SME-COMMON/Example9-SumInv1andO.csv T
+ 
+java -classpath core-japan-0.0.1.jar wuwei.japan_core.cius.Csv2invoice SME-COMMON data/csv/JP-PINT/Example.csv data/xml/SME-COMMON2Example.xml T
+java -classpath core-japan-0.0.1.jar wuwei.japan_core.cius.Csv2invoice SME-COMMON data/csv/JP-PINT/Example1-minimum.csv data/xml/SME-COMMON2Example1-minimum.xml T
+java -classpath core-japan-0.0.1.jar wuwei.japan_core.cius.Csv2invoice SME-COMMON data/csv/JP-PINT/Example2-TaxAcctCur.csv data/xml/SME-COMMON2Example2-TaxAcctCur.xml T
+java -classpath core-japan-0.0.1.jar wuwei.japan_core.cius.Csv2invoice SME-COMMON data/csv/JP-PINT/Example3-SumInv1.csv data/xml/SME-COMMON2Example3-SumInv1.xml T
+java -classpath core-japan-0.0.1.jar wuwei.japan_core.cius.Csv2invoice SME-COMMON data/csv/JP-PINT/Example4-SumInv2.csv data/xml/SME-COMMON2Example4-SumInv2.xml T
+java -classpath core-japan-0.0.1.jar wuwei.japan_core.cius.Csv2invoice SME-COMMON data/csv/JP-PINT/Example5-AllowanceCharge.csv data/xml/SME-COMMON2Example5-AllowanceCharge.xml T
+java -classpath core-japan-0.0.1.jar wuwei.japan_core.cius.Csv2invoice SME-COMMON data/csv/JP-PINT/Example6-CorrInv.csv data/xml/SME-COMMON2Example6-CorrInv.xml T
+java -classpath core-japan-0.0.1.jar wuwei.japan_core.cius.Csv2invoice SME-COMMON data/csv/JP-PINT/Example7-Return.Quan.ItPr.csv data/xml/SME-COMMON2Example7-Return.Quan.ItPr.xml T
+java -classpath core-japan-0.0.1.jar wuwei.japan_core.cius.Csv2invoice SME-COMMON data/csv/JP-PINT/Example9-SumInv1andO.csv data/xml/SME-COMMON2Example9-SumInv1andO.xml T
+
+
+# Example-TaxAcctCur
+java -classpath core-japan-0.0.1.jar wuwei.japan_core.cius.Invoice2csv JP-PINT data/xml/JP-PINT/Example-TaxAcctCur.xml data/csv/JP-PINT/Example-TaxAcctCur.csv T
+java -classpath core-japan-0.0.1.jar wuwei.japan_core.cius.Csv2invoice JP-PINT data/csv/JP-PINT/Example-TaxAcctCur.csv data/xml/JP-PINT2/Example-TaxAcctCur.xml T
+java -classpath core-japan-0.0.1.jar wuwei.japan_core.cius.Invoice2csv SME-COMMON data/xml/JP-PINT/Example-TaxAcctCur.xml data/csv/SME-COMMON/Example-TaxAcctCur.csv T
+java -classpath core-japan-0.0.1.jar wuwei.japan_core.cius.Csv2invoice SME-COMMON data/csv/JP-PINT/Example-TaxAcctCur.csv data/xml/SME-COMMON2Example-TaxAcctCur.xml T
+
