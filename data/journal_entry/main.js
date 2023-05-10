4370d06e9bd41a1d2943ea1fa178beb9c1a499ab
SAMBUICHI, Nobuyuki <nobuyuki@sambuichi.jp>
2023-05-10 10:11:56 +0900

    Update main.js

--- data/journal_entry/main.js
+++ data/journal_entry/main.js
@@ -1,24 +1,58 @@
-// main.js
-var csvUrl = 'https://example.com/data.csv';
-var xhr = new XMLHttpRequest();
-xhr.onreadystatechange = function() {
-    if (xhr.readyState === 4 && xhr.status === 200) {
-        var csvData = xhr.responseText;
-        var rows = csvData.split('\n').map(function(row) {
-            return row.split(',');
-        });
-        var table = document.getElementById('csv-table');
-        var tbody = table.querySelector('tbody');
-        rows.forEach(function(row) {
-            var tr = document.createElement('tr');
-            row.forEach(function(cell) {
-                var td = document.createElement('td');
-                td.textContent = cell;
-                tr.appendChild(td);
-            });
-            tbody.appendChild(tr);
-        });
-    }
-};
-xhr.open('GET', csvUrl, true);
-xhr.send();
+<!DOCTYPE html>
+<html>
+<head>
+    <title>CSVデータを加工して表示する</title>
+</head>
+<body>
+    <table id="myTable">
+        <thead>
+            <tr>
+                <th>商品名</th>
+                <th>価格</th>
+                <th>数量</th>
+                <th>合計</th>
+            </tr>
+        </thead>
+        <tbody>
+        </tbody>
+    </table>
+    <script>
+        // XMLHttpRequestオブジェクトを使用して、CSVファイルを取得する
+        var xhr = new XMLHttpRequest();
+        xhr.open('GET', 'data.csv', true);
+        xhr.onload = function() {
+            // 取得したCSVデータをパースして、JavaScriptの配列に変換する
+            var data = xhr.responseText.split('\n');
+            var items = [];
+            for (var i = 0; i < data.length; i++) {
+                var item = data[i].split(',');
+                items.push(item);
+            }
+
+            // 配列の内容を加工して、HTML要素に追加して表示する
+            var tableBody = document.querySelector('#myTable tbody');
+            for (var i = 0; i < items.length; i++) {
+                var item = items[i];
+                var price = parseInt(item[1], 10);
+                var quantity = parseInt(item[2], 10);
+                var total = price * quantity;
+                var tr = document.createElement('tr');
+                var td1 = document.createElement('td');
+                var td2 = document.createElement('td');
+                var td3 = document.createElement('td');
+                var td4 = document.createElement('td');
+                td1.textContent = item[0];
+                td2.textContent = item[1];
+                td3.textContent = item[2];
+                td4.textContent = total;
+                tr.appendChild(td1);
+                tr.appendChild(td2);
+                tr.appendChild(td3);
+                tr.appendChild(td4);
+                tableBody.appendChild(tr);
+            }
+        };
+        xhr.send();
+    </script>
+</body>
+</html>
