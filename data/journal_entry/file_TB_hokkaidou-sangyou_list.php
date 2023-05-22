24f1848060a07b8db3e87920b65e07e416602b22
SAMBUICHI, Nobuyuki <nobuyuki@sambuichi.jp>
2023-05-22 17:15:28 +0900

    Update file_TB_hokkaidou-sangyou_list.php

--- data/journal_entry/file_TB_hokkaidou-sangyou_list.php
+++ data/journal_entry/file_TB_hokkaidou-sangyou_list.php
@@ -1,8 +1,6 @@
 <?php
   $dir = __DIR__ . '/' . 'XBRL_GLinstances'; // ファイル一覧を取得するディレクトリ
   $files = array_diff(scandir($dir), array('.', '..')); // ディレクトリ内のファイル一覧を取得
-  // header('Content-Type: application/json'); // レスポンスのContent-TypeをJSONに設定
-  // echo json_encode($files); // ファイル名の配列をJSON形式で出力
   header('Content-Type: application/json; charset=UTF-8');
   echo json_encode($files, JSON_UNESCAPED_UNICODE);
 ?>
