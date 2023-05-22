5804f95e262fb3465847507fb06ed3b946572b21
Nobuyuki SAMBUICHI <nobuyuki@sambuichi.jp>
2023-05-22 16:30:30 +0900

    Update file_list.php

--- data/journal_entry/file_list.php
+++ data/journal_entry/file_list.php
@@ -1,10 +1,5 @@
 <?php
-  // dirの引数を取得
-  if (isset($_GET['dir'])) {
-    $name = $_GET['dir'];
-  } else {
-    $dir = './XBRL_GLinstances'; // ファイル一覧を取得するディレクトリ
-  }
+  $dir = __DIR__ . '/' . 'XBRL_GLinstances'; // ファイル一覧を取得するディレクトリ
   $files = array_diff(scandir($dir), array('.', '..')); // ディレクトリ内のファイル一覧を取得
   header('Content-Type: application/json'); // レスポンスのContent-TypeをJSONに設定
   echo json_encode($files); // ファイル名の配列をJSON形式で出力
