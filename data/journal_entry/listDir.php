8a88d11f8f470a9ed7c9927837cc4dec5262d620
SAMBUICHI, Nobuyuki <nobuyuki@sambuichi.jp>
2023-05-22 14:56:23 +0900

    Update listDir.php

--- data/journal_entry/listDir.php
+++ data/journal_entry/listDir.php
@@ -2,7 +2,7 @@
   // dirの引数を取得
   $dir = isset($_GET['dir']) ? $_GET['dir'] : ''; // パラメータが存在しない場合は空文字列をデフォルト値とする
 
-  if (preg_match('/^[\-\_\.\/a-zA-Z0-9]+$/', $dir)) {
+  if (preg_match('/^[a-zA-Z0-9\/-]+$/', $dir)) {
     // 正規表現にマッチする場合の処理
     $files = array_diff(scandir($dir), array('.', '..')); // ディレクトリ内のファイル一覧を取得
     header('Content-Type: application/json'); // レスポンスのContent-TypeをJSONに設定
