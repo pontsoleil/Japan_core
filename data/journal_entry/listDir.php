0b72ed5db2f9cf9221ad69cebb9d05794c635ef2
SAMBUICHI, Nobuyuki <nobuyuki@sambuichi.jp>
2023-05-22 15:45:09 +0900

    Update listDir.php

--- data/journal_entry/listDir.php
+++ data/journal_entry/listDir.php
@@ -1,19 +1,16 @@
 <?php
-if (isset($_GET['dir'])) {
-    $dir = $_GET['dir'];
-    $absolutePath = __DIR__ . '/' . $dir;
+$dir = isset($_GET['dir']) ? $_GET['dir'] : '';
 
-    if (is_dir($absolutePath)) {
-        $files = scandir($absolutePath);
-        $fileList = array_diff($files, array('.', '..'));
-
-        header('Content-Type: application/json');
-        echo json_encode($fileList);
-    } else {
-        echo '指定されたディレクトリが存在しません。';
-    }
-} else {
+if ($dir === '') {
     echo 'ディレクトリが指定されていません。';
+} elseif (preg_match('/^[a-zA-Z0-9\/-]+$/', $dir)) {
+    $dirPath = __DIR__ . '/' . $dir;
+    $files = array_diff(scandir($dirPath), array('.', '..'));
+
+    header('Content-Type: application/json');
+    echo json_encode($files);
+} else {
+    echo '無効なパラメータです。指定できるディレクトリ名は英数字及び_-のみです。';
 }
 ?>
 
