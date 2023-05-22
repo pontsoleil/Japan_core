b5e5d01b177305d17c6f9c982bbad6c633620a57
SAMBUICHI, Nobuyuki <nobuyuki@sambuichi.jp>
2023-05-22 10:36:20 +0900

    Update main.js

--- data/journal_entry/js/main.js
+++ data/journal_entry/js/main.js
@@ -840,12 +840,12 @@ main = (function () {
             let sourceSelect = document.querySelector('#source');
             var source = sourceSelect.value;
             if ('xbrl-gl'==source) {
-                document.querySelector('table#horizontal').addClass('d-none')
-                document.querySelector('table#horizontal2').removeClass('d-none')
+                document.querySelector('table#horizontal').classList.remove('d-none');
+                document.querySelector('table#horizontal2').classList.add('d-none');
                 getHorizontal();
             } else if ('hokkaidou-sangyou'==source) {
-                document.querySelector('table#horizontal').removeClass('d-none')
-                document.querySelector('table#horizontal2').addClass('d-none')
+                document.querySelector('table#horizontal').classList.add('d-none');
+                document.querySelector('table#horizontal2').classList.remove('d-none');
                 getHorizontal2();
             }
         });
